2b2e6bbb82ba05e42381980ebb3fbcdf0c745ebd
Boondock19 <josematiasgonzalez20@gmail.com>
2022-08-12 16:28:42 -0400

    Creados componentes y servicios base

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TableComponent } from './components/table/table.component';
+import { NewPokemonComponent } from './components/new-pokemon/new-pokemon.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    TableComponent,
+    NewPokemonComponent
+  ],
   imports: [
     CommonModule
   ]
